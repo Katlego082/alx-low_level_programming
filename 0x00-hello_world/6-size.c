9b3c6ff341bd8c7773e1f1ea6b3a7958ff7f792d
Katlego Molopa <katlego.molopa.082@gmail.com>
2023-02-16 07:27:29 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: 0 (success)
+ */
+int main(void)
+{
+printf("Size of a char: %i byte(s)\n", sizeo f(char));
+printf("size of an int: %i byte(s)\n", sizeo f(int));
+printf("size of a long int: %i byte(s)\n", sizeo f(long int));
+printf("size of a long long int: %i byte(s)\n", sizeo f(long long int));
+printff("size of a float: %i byte(s)\n", sizeo f(float));
+return (0);
+}
