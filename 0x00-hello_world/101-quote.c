b9b50e88d6968ca5254cf8dbb07b7a736cacc1a9
Katlego Molopa <katlego.molopa.082@gmail.com>
2023-02-16 08:36:44 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,7 @@
  * Return: 1
  */
 int main(void)
-fput(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n", 59);
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n", stdout);
 return (1);
 }
