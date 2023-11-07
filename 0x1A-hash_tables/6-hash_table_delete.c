124894e9bfc1dec9b6a6e83ed96da0e1e0903ca1
katlego molopa <katlego.molopa.082@gmail.com>
2023-11-07 10:32:59 -0800

    hash tables

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,53 +1,30 @@
-#include <stdlib.h>
+#include "hash_tables.h"
 
-// Define a structure for a key-value pair
-typedef struct {
-    char* key;
-    char* value;
-} KeyValuePair;
+/**
+ * hash_table_delete - Deletes a hash table.
+ * @ht: A pointer to a hash table.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_table_t *head = ht;
+	hash_node_t *node, *tmp;
+	unsigned long int i;
 
-// Define a structure for a hash table
-typedef struct {
-    int size;           // Number of buckets
-    KeyValuePair** table; // Array of buckets
-} hash_table_t;
-
-// Function to delete a key-value pair
-void delete_key_value_pair(KeyValuePair* kv) {
-    if (kv) {
-        free(kv->key);
-        free(kv->value);
-        free(kv);
-    }
-}
-
-// Function to delete a hash table
-void hash_table_delete(hash_table_t *ht) {
-    if (ht) {
-        for (int i = 0; i < ht->size; i++) {
-            KeyValuePair* current = ht->table[i];
-            while (current) {
-                KeyValuePair* next = current->next;
-                delete_key_value_pair(current);
-                current = next;
-            }
-        }
-
-        free(ht->table);
-        free(ht);
-    }
-}
-
-int main() {
-    // Create a hash table
-    hash_table_t* ht = (hash_table_t*)malloc(sizeof(hash_table_t));
-    ht->size = 10;
-    ht->table = (KeyValuePair**)malloc(sizeof(KeyValuePair*) * ht->size);
-
-    // Populate the hash table with data
-
-    // Delete the hash table and its contents
-    hash_table_delete(ht);
-
-    return 0;
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				tmp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp;
+			}
+		}
+	}
+	free(head->array);
+	free(head);
 }
